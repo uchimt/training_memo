6e115dae219e7dbb9cf885bdfef5beae9b1f4388
uchimt <dosanco.ucchi@gmail.com>
2020-09-06 22:01:47 +0900

    ログイン画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # userログイン/ログアウト用のルーティング
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :home, only: [:index]
+  
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
